647d5ef434ef58f58caa90a48b1d09ff274fa5d4
Naoki051 <henriquenaoki051@gmail.com>
2024-10-20 22:24:41 -0300

    update revertendo problemas de exibição

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -12,27 +12,17 @@ exports.login = async (req, res) => {
     const user = await User.findByNomeUser(nome_user);
 
     if (!user) {
-      return res.status(401).json({ message: 'Credenciais inválidas' });
+      return res.status(401).json({ message: 'Usuário não encontrado' });
     }
 
     const isMatch = await user.verifyPassword(senha);
 
     if (!isMatch) {
-      return res.status(401).json({ message: 'Credenciais inválidas' });
+      return res.status(401).json({ message: 'Senha incorreta' });
     }
 
     const token = jwt.sign({ userId: user.id_user }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    // Configurar o cookie HTTP-only com o token
-    res.cookie('auth_token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Use HTTPS em produção
-      sameSite: 'strict',
-      maxAge: 3600000 // 1 hora em milissegundos
-    });
-
-    // Enviar resposta de sucesso sem expor o token
-    res.json({ message: 'Login realizado com sucesso' });
+    res.json({ token });
   } catch (error) {
     console.error('Erro na autenticação:', error);
     res.status(500).json({ message: 'Erro no servidor' });
